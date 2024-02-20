4f2f49e61a514a740380cfbe3d662e4dbb9f1a2a
P H <git@hoffmanscloud.com>
2024-02-19 23:35:15 -0500

    Tweaks build process to use jq to query configuration files.

--- install.sh
+++ install.sh
@@ -1,54 +1,63 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euox pipefail
+
+# Read packages for installation from json config.
+readarray -t PACKAGES < <(jq -rc '.packages[]' /tmp/$CONFIG)
+
+# Read containers for use from json config.
+readarray -t CONTAINERS < <(jq -rc '.containers[]' /tmp/$CONFIG)
+
+# Read SELinux boolean values from json config.
+readarray -t SELINUX_BOOLEANS < <(jq -rc '.selinux.booleans[]' /tmp/$CONFIG)
 
 # Remove containers, networks, services, & timers
 pushd /etc/containers/systemd/ &> /dev/null
-if [ "${INCLUDE_AUDIOBOOKSHELF-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]audiobookshelf[[:space:]] ]]; then
   rm audiobookshelf.container
 fi
-if [ "${INCLUDE_BEETS-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]beets[[:space:]] ]]; then
   rm beets.container
 fi
-if [ "${INCLUDE_EMBY-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]emby[[:space:]] ]]; then
   rm embyserver.container
 fi
-if [ "${INCLUDE_GOTIFY-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]gotify[[:space:]] ]]; then
   rm gotify.container
 fi
-if [ "${INCLUDE_JELLYFIN-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]jellyfin[[:space:]] ]]; then
   rm jellyfin.container
 fi
-if [ "${INCLUDE_LUBELOGGER-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]lubelogger[[:space:]] ]]; then
   rm lubelogger.container
 fi
-if [ "${INCLUDE_MEALIE-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]mealie[[:space:]] ]]; then
   rm mealie.container
 fi
-if [ "${INCLUDE_MINIO-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]minio[[:space:]] ]]; then
   rm minio.container
 fi
-if [ "${INCLUDE_NEXTCLOUD-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]nextcloud[[:space:]] ]]; then
   rm nextcloud*.container
   pushd /etc/systemd/system/ &> /dev/null
   rm nextcloud*.{service,timer}
   popd &> /dev/null
 fi
-if [ "${INCLUDE_PLEX-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]plex[[:space:]] ]]; then
   rm plex*.{container,network}
 fi
-if [ "${INCLUDE_PMM-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]pmm[[:space:]] ]]; then
   if [ -f plexmetamanager.container ]; then
     rm plexmetamanager.container
   fi
 fi
-if [ "${INCLUDE_SYNCTHING-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]syncthing[[:space:]] ]]; then
   rm syncthing.container
 fi
-if [ "${INCLUDE_UNIFI-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]unifi[[:space:]] ]]; then
   rm unifi*.{container,network}
 fi
-if [ "${INCLUDE_YTSUBS-}" != "y" ]; then
+if [[ ! " ${CONTAINERS[*]} " =~ [[:space:]]ytsubs[[:space:]] ]]; then
   rm ytsubs.container
   rm /etc/systemd/system/ytsubs.timer
 fi
@@ -57,14 +66,12 @@ popd &> /dev/null
 # Override nfs to provide full utilities
 rpm-ostree override remove nfs-utils-coreos --install nfs-utils
 
-# Install some packages
-INCLUDED_PACKAGES=( borgbackup curl dbus-tools firewalld iwlegacy-firmware iwlwifi-dvm-firmware )
-INCLUDED_PACKAGES+=( iwlwifi-mvm-firmware just kernel-modules-extra nano pciutils podman rclone )
-INCLUDED_PACKAGES+=( rsync-daemon samba setroubleshoot vim wget xdg-dbus-proxy xdg-user-dirs )
-rpm-ostree install "${INCLUDED_PACKAGES[@]}"
+# Install specified packages
+rpm-ostree install "${PACKAGES[@]}"
 
-# Configure samba
-setsebool -P samba_export_all_rw 1
-
-# Configure rsync
-setsebool -P rsync_full_access 1
+# Configure SELinux global booleans
+for BOOL in ${SELINUX_BOOLEANS[@]}; do
+  NAME=$(echo $BOOL | jq -r '.name')
+  VALUE=$(echo $BOOL | jq -r '.value')
+  setsebool -P "$NAME" "$VALUE"
+done
