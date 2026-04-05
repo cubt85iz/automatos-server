3cb0d4cceea77f4182947e3e18c0a27c87504c06
P H <git@cubt85iz>
2026-04-05 12:58:41 -0400

    Adds warning to inform users of change regarding container locations.

--- configure.sh
+++ configure.sh
@@ -8,8 +8,21 @@ readarray -t REPOS < <(jq -rc '.repos[]' /.config/$CONFIG)
 # Read packages for installation from json config.
 readarray -t PACKAGES < <(jq -rc '.packages[]' /.config/$CONFIG)
 
+# Read containers for use from json config.
+readarray -t CONTAINERS < <(jq -rc '.containers[]' /.config/$CONFIG)
+
 # Read SELinux boolean values from json config.
 readarray -t SELINUX_BOOLEANS < <(jq -rc '.selinux.booleans[]' /.config/$CONFIG)
+
+# Output warning if the user has provided a configuration containing containers.
+if [ "${#CONTAINERS[@]}" -gt 0 ]; then
+  echo "WARNING: The provided configuration includes containers. Historically, this would " \
+    "limit the list of provided container files to those specified. This is no longer the " \
+    "case and the complete set of containers will be provided for each image. Update your " \
+    "configuration to remove the specified containers and update your Butane configuration " \
+    "to use symlinks to point to the container files you wish to run. More information can " \
+    "be found in the automatos-server-config repository."
+fi
 
 # Override nfs to provide full utilities
 rpm-ostree override remove nfs-utils-coreos --install nfs-utils
